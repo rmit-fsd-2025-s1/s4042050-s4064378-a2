b5b82c5cd5568ff2ce16db2f75bb486e162724c3
don-roy-j <S4064378@student.rmit.edu.au>
2025-05-06 22:57:09 +1000

    add basic configurations to node app

---
+++ node-app/src/data-source.ts
@@ -0,0 +1,20 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { User } from "./entity/User";
+
+export const AppDataSource = new DataSource({
+  type: "mysql",
+  host: "209.38.26.237",
+  port: 3306,
+  username: "S4064378",
+  password: "Aa@2235023",
+  database: "S4064378",
+  // synchronize: true will automatically create database tables based on entity definitions
+  // and update them when entity definitions change. This is useful during development
+  // but should be disabled in production to prevent accidental data loss.
+  synchronize: true,
+  logging: true,
+  entities: [User],
+  migrations: [],
+  subscribers: [],
+});
