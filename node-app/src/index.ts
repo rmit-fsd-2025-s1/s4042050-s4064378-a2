a2ee30a4c5eaac81d4e62bf01165cba042a3f8cd
malithrukshankg75 <S4042050@student.rmit.edu.au>
2025-05-28 19:55:33 +1000

    End points added

--- node-app/src/index.ts
+++ node-app/src/index.ts
@@ -2,6 +2,8 @@ import "reflect-metadata";
 import express from "express";
 import { AppDataSource } from "./data-source";
 import userRoutes from "./routes/user.routes";
+import applicationsRouter from "./routes/applications";
+import courseRoutes from "./routes/courses";
 import cors from "cors";
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,6 +11,8 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 app.use("/teach_team", userRoutes);
+app.use("/api/applications", applicationsRouter);
+app.use("/api/courses", courseRoutes);
 
 AppDataSource.initialize()
   .then(() => {
