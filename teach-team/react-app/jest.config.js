7b4a692e72e0988ed3ee327955f4f104492264ad
malithrukshankg75 <S4042050@student.rmit.edu.au>
2025-06-08 21:25:14 +1000

    updated test

--- teach-team/react-app/jest.config.js
+++ teach-team/react-app/jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    transform: {
-      '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
-    },
-    moduleNameMapper: {
-      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    },
-    setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-  };
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+};
